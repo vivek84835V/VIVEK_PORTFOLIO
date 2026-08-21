25366846c920563eca0defe41c85dd971f85a159
vivek shimpi <your.vivekshimpi146@gmail.com.com>
2026-08-21 13:19:10 +0530

    setup cors

--- Backend/app.js
+++ Backend/app.js
@@ -11,9 +11,12 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
